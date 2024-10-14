5c2567d8dfca68a6783ef15ae4aee8d30da24d4f
Pratik Bhowmik <pratikbhowmik@Pratiks-MacBook-Air.local>
2024-10-14 07:14:15 +0530

    Fancode assessment

---
+++ cypress/e2e/APItests.cy.js
@@ -0,0 +1,25 @@
+describe('Fancode API automation test', () => {
+  it('tests the API', () => {
+    cy.request('https://jsonplaceholder.typicode.com/users')
+      .then((response) => {
+        expect(response.status).to.eq(200);
+        const users = response.body;
+
+        //Filter users based on city Fancode
+        const fanCodeUsers = users.filter(user => user.city === 'FanCode');
+
+        fanCodeUsers.forEach(user => {
+
+          const todos = user.todos;
+          const completedTodos = todos.filter(todo => todo.completed === true);
+
+          //Calculate completed percentage
+          const completedPercentage = (completedTodos.length / todos.length) * 100;
+
+          //Assert that percentage is greater than 50%
+          expect(completedPercentage).to.be.greaterThan(50);
+
+        });
+      });
+  });
+});
